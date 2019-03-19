16341c9695db4d8a851efbaa03665c21da6842a1
Murtaza Safarpour <murtazababurtech2018@gmail.com>
2019-03-19 13:17:47 +0300

    Initial Commit

---
+++ DatingApp.API/DataContext/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.DataContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
